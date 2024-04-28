39cba5cf8a603341f774cb888811a2be8cc01d3f
GIGACHAD <denkingbad@gmail.com>
2024-04-28 21:37:31 +0700

    Initial commit

---
+++ 2.3 sep_end/sep_end.py
@@ -0,0 +1,7 @@
+first_word = 'Бесконечность'
+second_word = 'Не'
+third_word = 'Предел'
+
+asterisk = "*"
+
+print(first_word, second_word, third_word, sep=asterisk, end="!")  # Бесконечность*Не*Предел!
