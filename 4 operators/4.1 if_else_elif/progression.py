e73b091e333964110ef70310c23c23044725c16b
3dendroid <denkingbad@gmail.com>
2024-05-06 20:25:47 +0700

    add code in progression.py

--- 4 operators/4.1 if_else_elif/progression.py
+++ 4 operators/4.1 if_else_elif/progression.py
@@ -1,5 +1,9 @@
 first_a = int(input())
 second_a = int(input())
-thrid_a = int(input())
+third_a = int(input())
 
 ###CONTINUE
+if third_a - second_a == second_a and second_a - first_a == first_a:
+    print('YES')
+else:
+    print('NO')
