6d14872749c5ff84d9b55e99ca5a6d9a165d9093
3dendroid <denkingbad@gmail.com>
2024-05-07 16:42:27 +0700

    finish 4.1, next 4.2

--- 4 operators/4.1 if_else_elif/age_group.py
+++ 4 operators/4.1 if_else_elif/age_group.py
@@ -1,1 +1,13 @@
-# CONTINUE
+i = int(input())
+
+try:
+    if i <= 13:
+        print('детство')
+    elif 14 <= i <= 24:
+        print('молодость')
+    elif 25 <= i <= 59:
+        print('зрелость')
+    else:
+        print('старость')
+except ValueError as e:
+    print(e)
---
+++ 4 operators/4.1 if_else_elif/only_positive.py
@@ -0,0 +1,21 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+sum = 0
+
+if a >= 0:
+    sum += a
+else:
+    a = 0
+
+if b >= 0:
+    sum += b
+else:
+    b = 0
+if c >= 0:
+    sum += c
+else:
+    c = 0
+
+print(a + b + c)
