68d5a27b337b2134c43e4d285b645bfbb7bdd289
3dendroid <denkingbad@gmail.com>
2024-05-11 19:45:50 +0700

    add three scripts, next 4.3

---
+++ 4 operators/4.2 logic_operations/chess.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a == c or (c == d and a == d):
+    print('YES')
+elif b == d or (c == d and a == d):
+    print('YES')
+else:
+    print('NO')
---
+++ 4 operators/4.2 logic_operations/chess_2.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if a - 1 <= c <= a + 1 and b - 1 <= d <= b + 1 and (1 <= a <= 8 and 1 <= b <= 8 and 1 <= c <= 8 and 1 <= d <= 8):
+    print('YES')
+else:
+    print('NO')
