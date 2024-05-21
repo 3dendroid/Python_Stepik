c1c527b16b25e2cdc78fbf5be00881f7d6f3f4ea
3dendroid <denkingbad@gmail.com>
2024-05-21 17:05:08 +0700

    did crosslines.py, next will be exam

--- 4 operators/4.3 cascades/crosslines.py
+++ 4 operators/4.3 cascades/crosslines.py
@@ -1,7 +1,46 @@
-a1 = int(input())
-b1 = int(input())
-a2 = int(input())
-b2 = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if b < c or d < a:
+    print("пустое множество")
+elif b == c:
+    print(b)
+elif d == a:
+    print(d)
+elif a <= c and b <= d:
+    print(c, b)
+elif c <= a and d <= b:
+    print(a, d)
+elif a == c and b <= d:
+    print(a, b)
+elif c <= a and b >= d:
+    print(a, b)
+elif c <= a and b == d:
+    print(b, d)
+elif a <= c and d >= b:
+    print(c, b)
+elif a <= c and d >= b:
+    print(a, d)
+elif a <= c and d == b:
+    print(d, c)
+elif a == b and c == d:
+    print(a, d)
 
 
-# continue
+
+# working code
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+
+# if b < c or d < a:
+#     print("пустое множество")
+# elif b == c:
+#     print(b)
+# elif d == a:
+#     print(d)
+# else:
+#     print(max(a, c), min(b, d))
