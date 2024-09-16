96720be601f4e87ac87bbf5d781402bb16a7d571
denis.sazonov <denis.sazonov@mytona.com>
2024-09-16 16:28:59 +0700

    next code_review_3.py

---
+++ 7 for_and_while_loops/7.7 error_search_and_code_review/code_review_2.py
@@ -0,0 +1,16 @@
+mx = -1000000
+s = 0
+
+for i in range(0, 10):
+    x = int(input())
+
+    if x < 0:
+        s += x
+
+        if x > mx:
+            mx = x
+if s == 0:
+    print('NO')
+else:
+    print(s)
+    print(mx)
