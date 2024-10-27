7e6bf37443fe42238ead1a36a353dc2bc2b45c35
3dendroid <denkingbad@gmail.com>
2024-10-27 18:50:42 +0700

    next examples.py in 9.5

---
+++ 9 string_data_types/9.4 string_methods_2/deleting_fragment.py
@@ -0,0 +1,6 @@
+s = input ()
+
+first = s.find ('h')
+last = s.rfind ('h')
+
+print (s[:first] + s[last + 1:])
--- 9 string_data_types/9.4 string_methods_2/first_and_last_entry.py
+++ 9 string_data_types/9.4 string_methods_2/first_and_last_entry.py
@@ -1,3 +1,7 @@
 s = input ()
-
-# next
+if 'f' not in s:
+    print ('NO')
+elif s.count ('f') == 1:
+    print (s.find ('f'))
+else:
+    print (s.find ('f'), s.rfind ('f'))
