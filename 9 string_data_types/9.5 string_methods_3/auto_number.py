31611eac7b222e81b76a02b2cd129aef9173a2a8
3dendroid <denkingbad@gmail.com>
2024-11-02 20:44:45 +0700

    next check_nickname.py

--- 9 string_data_types/9.5 string_methods_3/auto_number.py
+++ 9 string_data_types/9.5 string_methods_3/auto_number.py
@@ -1,1 +1,11 @@
-# next
+s = str (input ())
+
+digits = '0123456789'
+words = 'АВЕКМНОРСТУХ'
+space = '_'
+
+if ((len (s) == 10 and s[9] in digits or len (s) == 9) and s[6] == space and s[0] in words and s[4] in words and s[
+    5] in words):
+    print ('YES')
+else:
+    print ('NO')
