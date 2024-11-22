741c1cc61a53c8c619ed597cf2991631cb07d034
3dendroid <denkingbad@gmail.com>
2024-11-22 16:17:33 +0700

    next Increasing the cost of the response

--- 9 string_data_types/9.7 strings_in_computer/response_cost.py
+++ 9 string_data_types/9.7 strings_in_computer/response_cost.py
@@ -1,1 +1,8 @@
-# next
+response = input ()
+cost = 0
+bee = chr (128029)
+
+for i in range (len (response)):
+    cost += ord (response[i]) * 3
+print (f"Текст сообщения: '{response}'")
+print (f'Стоимость сообщения: {cost}{bee}')
