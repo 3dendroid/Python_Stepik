a23233eedd5e865510a499313033b4d55dbe50cb
3dendroid <denkingbad@gmail.com>
2024-12-04 11:48:29 +0700

    next str_max_and_min.py

--- 9 string_data_types/9.8 string_compare/examples.py
+++ 9 string_data_types/9.8 string_compare/examples.py
@@ -1,1 +1,31 @@
-# next
+# print ('a' > 'b')
+# print ('a' < 'z')
+#
+# print (ord ('a') > ord ('b'))
+# print (ord ('a') < ord ('z'))
+#
+# print ('Тинькофф' == 'Т-банк')
+# print ('¥' > '$€£')
+# print (max ('🐷', '🦆', '🐔'))
+#
+# print (max ('tree', 'try', 'true'))
+# print (min ('cat', 'car', 'cape'))
+
+# a, b = input (), input ()
+# print ((a == b) * '==', (a > b) * '>', (a < b) * '<')
+
+print (max ('aunt', 'a', 'an', 'An'))
+print (min ('aunt', 'a', 'an', 'An'))
+
+lst = ['a11', 'a9', 'a', 'a10']
+lst.sort ()
+print (lst)
+
+print (max (9, 10, 11))
+print (max ('9', '10', '11'))
+
+print (min ('9', '10', '11') + max ('9', '10', '11'))
+
+# print (min (10, 5, 15) + max ('10', '5', '15'))
+
+print (min (10, 5, 15) + max (10, 5, '15'))
