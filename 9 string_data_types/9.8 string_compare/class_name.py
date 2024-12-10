e6e524e9c2e7a84d6299d735366c9c7a4511b51b
3dendroid <denkingbad@gmail.com>
2024-12-10 11:35:37 +0700

    next unusual_comparison.py

--- 9 string_data_types/9.8 string_compare/class_name.py
+++ 9 string_data_types/9.8 string_compare/class_name.py
@@ -1,1 +1,9 @@
-# next
+letters = 'АБВГДЕЖЗИЙКЛМНОП'
+digits = '0123456789'
+
+for i in range (int (input ())):
+    s = input ().upper ()
+    if len (s) == 2 and s[0] in digits and s[1] in letters:
+        print ('YES')
+    else:
+        print ('NO')
