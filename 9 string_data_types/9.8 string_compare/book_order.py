e21c90f890f4e802e2239c51412ee9435df2e569
3dendroid <denkingbad@gmail.com>
2024-12-16 13:17:48 +0700

    next 10 chapter, exams

--- 9 string_data_types/9.8 string_compare/book_order.py
+++ 9 string_data_types/9.8 string_compare/book_order.py
@@ -1,1 +1,16 @@
-# next
+list1 = []
+list2 = []
+n = int (input ())
+
+for i in range (n):
+    c = input ().strip ()
+
+    list1.append (c[:c.find ('')] + c[c.find ('«'):])
+    list2.append (c[:c.find ('')] + c[c.find ('«'):])
+
+list2.sort ()
+
+if list1 == list2:
+    print ('NO')
+else:
+    print ('YES')
