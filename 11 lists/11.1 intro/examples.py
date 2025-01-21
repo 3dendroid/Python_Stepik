474f0e6d5093d7d59af5260c5c6a6d64e061bef6
Denis Sazonov <denis.sazonov@mytona.com>
2025-01-21 11:04:15 +0700

    next 11.2 folder

--- 11 lists/11.1 intro/examples.py
+++ 11 lists/11.1 intro/examples.py
@@ -1,1 +1,31 @@
-# next
+# numbers = [2, 4, 6, 8, 10]
+# languages = ['Python', 'C#', 'C++', 'Java']
+# info = ['Timur', 1992, 61.5]
+#
+# mylist = []  # пустой список
+# mylist = list()  # тоже пустой список
+
+# numbers = list(range(5))
+# print(numbers)
+
+# even_numbers = list(range(0, 10, 2))  # список содержит четные числа 0, 2, 4, 6, 8
+# odd_numbers = list(range(1, 10, 2))  # список содержит нечетные числа 1, 3, 5, 7, 9
+
+# print(even_numbers)
+# print(odd_numbers)
+
+# s = 'denis'
+# chars = list(s)
+# print(chars)
+
+# numbers = [3, 5, 7, 9]
+# print(numbers)
+
+# numbers = [1, 100, 7, 20, 17, 37, 22]
+# print(numbers)
+
+# numbers = [0, 1, 3, 14, 2, 7, 9, 8, 10]
+# print(numbers)
+
+names = ['Michael', 'John', 'Freddie']
+print(names)
---
+++ 11 lists/11.1 intro/list_of_words.py
@@ -0,0 +1,7 @@
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+       'w', 'x', 'y', 'z']
+print(abc[0:int(input())])
+
+# n = int(input())
+# s = 'abcdefghijklmnopqrstuvwxyz'
+# print(list(s[:n]))
