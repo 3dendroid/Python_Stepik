83799ea8ac94b72671d32ec5d63878579ec844aa
Denis Sazonov <denis.sazonov@mytona.com>
2025-01-27 15:51:23 +0700

    next list_of_lines.py

--- 11 lists/11.3 append/examples.py
+++ 11 lists/11.3 append/examples.py
@@ -1,1 +1,34 @@
-# next
+# numbers = [1, 1, 2, 3, 5, 8, 13]  # создаем список
+# numbers.append(21)  # добавляем число 21 в конец списка
+# numbers.append(34)  # добавляем число 34 в конец списка
+# print(numbers)
+#
+# numbers = []  # создаем пустой список
+# numbers.append(1)
+# numbers.append(2)
+# numbers.append(3)
+# print(numbers)
+
+# numbers = [0, 2, 4, 6, 8, 10]
+# odds = [1, 3, 5, 7]
+# numbers.extend(odds)
+# print(numbers)
+
+# words1 = ['iq option', 'stepik', 'beegeek']
+# words2 = ['iq option', 'stepik', 'beegeek']
+# words1.append('python')
+# words2.extend('python')
+# print(words1)
+# print(words2)
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# del numbers[5]  # удаляем элемент, имеющий индекс 5
+# print(numbers)
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# del numbers[2:7]  # удаляем элементы с 2 по 6 включительно
+# print(numbers)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+del numbers[::2]
+print(numbers)
---
+++ 11 lists/11.3 append/examples_2.py
@@ -0,0 +1,18 @@
+# names = []
+# names.append('Chromatica')
+# print(names)
+
+# numbers = [4, 2, 8, 6, 5]
+# numbers.append(7)
+# numbers.append(1)
+# print(numbers)
+
+# numbers = [4, 2]
+# numbers.extend([1, 2, 3])
+# numbers.extend([7, 17, 777])
+# print(numbers)
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'magenta']
+del colors[2]
+del colors[6]
+print(colors)
