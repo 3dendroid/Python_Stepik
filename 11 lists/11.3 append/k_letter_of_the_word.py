c1e4ae845dc93a23f932d600742b81d548721641
Denis Sazonov <denis.sazonov@mytona.com>
2025-02-06 11:35:08 +0700

    NEXT: examples.py

--- 11 lists/11.3 append/k_letter_of_the_word.py
+++ 11 lists/11.3 append/k_letter_of_the_word.py
@@ -1,1 +1,8 @@
-# NEXT
+n = int(input())
+s = [input() for _ in range(n)]
+k = int(input())
+
+for i in range(n):
+    x = []
+    x.extend(s[i])
+    print(x[k - 1] if k <= len(x) else '', end='')
---
+++ 11 lists/11.3 append/symbols_of_all_lines.py
@@ -0,0 +1,6 @@
+n = int(input())
+l = []
+
+for i in range(n):
+    l.extend(input())
+print(l)
