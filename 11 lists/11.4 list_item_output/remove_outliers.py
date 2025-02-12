14fad3fdb6a5c038797fffcab0a211b49bb410bf
Denis Sazonov <denis.sazonov@mytona.com>
2025-02-12 12:06:56 +0700

    NEXT: no_duplicates.py

--- 11 lists/11.4 list_item_output/remove_outliers.py
+++ 11 lists/11.4 list_item_output/remove_outliers.py
@@ -1,1 +1,18 @@
-#NEXT
+n = int(input())
+l = []
+
+# Чтение n чисел
+for _ in range(n):
+    l.append(int(input()))
+
+# Находим минимальное и максимальное значения
+min_value = min(l)
+max_value = max(l)
+
+# Удаляем минимальное и максимальное значения, сохраняя порядок
+for x in l:
+    if x != min_value and x != max_value:
+        print(x)
+
+# l = [x for x in l if x != min_value and x != max_value]
+
