65aad9b5e6ed03df1941aa048902d3b99773f5d0
Denis Sazonov <denis.sazonov@mytona.com>
2025-02-28 11:55:23 +0700

    NEXT: examples.py

---
+++ 11 lists/11.5 string_methods/add_separator.py
@@ -0,0 +1,9 @@
+text = input()
+print(input().join(text))
+
+# print(*input(), sep=input())
+
+# s, sep = input(), input()
+# print((sep).join(s))
+
+# print(*list(input()), sep=input())
--- 11 lists/11.5 string_methods/correct_ip_adress.py
+++ 11 lists/11.5 string_methods/correct_ip_adress.py
@@ -1,1 +1,15 @@
-# NEXT
+s = input().split('.')
+c = 0
+
+for i in s:
+    if 0 <= int(i) <= 255:
+        c += 1
+    else:
+        c = -1
+
+if c == 4:
+    print("ДА")
+else:
+    print("НЕТ")
+
+
