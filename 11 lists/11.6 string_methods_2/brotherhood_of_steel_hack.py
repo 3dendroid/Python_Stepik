c39d1fe49e6ea33699598c55e33254173f99d266
Denis Sazonov <denis.sazonov@mytona.com>
2025-03-10 12:31:05 +0700

    NEXT: examples.py in 11.7 folder

--- 11 lists/11.6 string_methods_2/brotherhood_of_steel_hack.py
+++ 11 lists/11.6 string_methods_2/brotherhood_of_steel_hack.py
@@ -1,1 +1,16 @@
-# NEXT
+# n = input()
+# for _ in range(int(n[1:])):
+#     s = input()
+#     if '#' in s:
+#         s = s[:s.find('#')]
+#     print(s.rstrip())
+
+# x = input()
+# x = x.replace(x[x.index("#")::], "").rstrip()
+
+numbers = [4, 2, 8, 6, 5, 3, 10, 4, 100, 1, -7]
+numbers.sort()
+del numbers[0]
+del numbers[-1]
+numbers.sort(reverse=True)
+print(numbers)
---
+++ 11 lists/11.6 string_methods_2/digit_sort.py
@@ -0,0 +1,6 @@
+n = [int(i) for i in input().split()]
+
+n.sort()
+print(*n)
+n.sort(reverse=True)
+print(*n)
