cc0658bc75d2d9f12664a474b0b78a9640c55e16
denis sazonov <denis.sazonov@mytona.com>
2025-03-12 13:16:28 +0700

    NEXT: list_expression_1.py

--- 11 lists/11.7 list_expressions/examples.py
+++ 11 lists/11.7 list_expressions/examples.py
@@ -1,1 +1,20 @@
-# NEXT
+# zeros = []
+# for i in range(10):
+#     zeros.append(0)
+
+# numbers = []
+# for i in range(10):
+#     numbers.append(i)
+
+# chars = [c for c in 'abcdefg']
+# print(chars)
+
+# n = int(input())
+# lines = [input() for _ in range(n)]
+# print(lines)
+
+# numbers = [i * j for i in range(1, 5) for j in range(2)]
+# print(numbers)
+
+# Как записывается списочное выражение?
+# [выражение for переменная in последовательность]
---
+++ 11 lists/11.7 list_expressions/examples_2.py
@@ -0,0 +1,24 @@
+# keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+#             'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+#             'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+# new_keywords = [x[1:] for x in keywords]
+# print(new_keywords)
+
+# keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+#             'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+#             'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+# lengths = [len(x) for x in keywords]
+# print(lengths)
+
+# keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif',
+#             'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+#             'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+# new_keywords = [x for x in keywords if len(x) >= 5]
+# print(new_keywords)
+
+# palindromes = [x for x in range(100, 1000) if x % 10 == x // 100]
+# print(palindromes)
+
+# palindromes = [x for x in range(100, 1000) if str(x) == str(x)[::-1]]
+# print(palindromes)
+
