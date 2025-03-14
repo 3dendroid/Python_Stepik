d59719c40d801aa267e7c36443996948b81df8bf
denis sazonov <denis.sazonov@mytona.com>
2025-03-14 14:17:20 +0700

    NEXT: in_one_line.py

--- 11 lists/11.7 list_expressions/list_expression_1.py
+++ 11 lists/11.7 list_expressions/list_expression_1.py
@@ -1,1 +1,5 @@
-# NEXT
+# print(*[i ** 2 for i in range(1, int(input()) + 1)], sep='\n')
+
+# x = int(input())
+# n = [i ** 2 for i in range(1, x + 1)]
+# print(*n, sep='\n')
---
+++ 11 lists/11.7 list_expressions/list_expression_2.py
@@ -0,0 +1,2 @@
+print(*[int(i) ** 3 for i in input().split()])
+
