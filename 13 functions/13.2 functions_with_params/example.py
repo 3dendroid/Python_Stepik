6f51b9d573f83c697a0fc13c43a32d83273e66fe
denis sazonov <denis.sazonov@mytona.com>
2025-04-07 16:12:34 +0700

    NEXT: star_triangle.py

--- 13 functions/13.2 functions_with_params/example.py
+++ 13 functions/13.2 functions_with_params/example.py
@@ -1,1 +1,39 @@
-# NEXT
+# def draw_box(height, width):  # функция принимает два параметра
+#     for i in range(height):
+#         print('*' * width)
+# draw_box(3, 3)
+# print()
+# draw_box(5, 5)
+# print()
+# draw_box(4, 10)
+
+# def draw_box(height, width):
+#     height = 2
+#     width = 10
+#     for i in range(height):
+#         print('*' * width)
+# n = 5
+# m = 7
+# draw_box(n, m)
+# print(n, m)
+
+# def print_number(a, b, c):
+#     d = (a + c) // b
+#     print(d)
+# print_number(2,3,11)
+
+# def change_us(a, b):
+#     a = 0
+#     b = 0
+#     print(a, b)
+# x = 1
+# y = 7
+# print(x, y)
+# change_us(x, y)
+# print(x, y)
+
+def print_text(text, num):
+    while num > 0:
+        print(text, end='')
+        num -= 1
+print_text('Python', 4)
