e4acecd5f9b91644638c3e41a79218232a2d1978
denis sazonov <denis.sazonov@mytona.com>
2025-04-11 11:33:04 +0700

    NEXT: examples.py in 13.4 folder

--- 13 functions/13.3 local_global_variables/examples.py
+++ 13 functions/13.3 local_global_variables/examples.py
@@ -1,1 +1,47 @@
-# NEXT
+# def print_texas():
+#     birds = 5000
+#     print('В Техасе обитает', birds, 'птиц.')
+# print_texas()
+
+# def print_texas():
+#     birds = 5000
+#     print('В Техасе обитает', birds, 'птиц.')
+# print_texas()
+#
+# def print_california():
+#     birds = 12000
+#     print('В Калифорнии обитает', birds, 'птиц.')
+# print_california()
+
+# def print_texas():
+#     print('В Техасе обитает', birds, 'птиц.')
+#     birds = 5000
+# print_texas()
+
+# def draw_box(height, width):
+#     for i in range(height):
+#         print('*' * width)
+# draw_box(3, 3)
+
+# def print_paris():
+#     print(s)
+#     s = 'I love Paris'
+# print_paris()
+
+# def print_paris():
+#     s = 'I love Paris'
+#     print(s)
+# def print_london():
+#     s = 'I love London'
+#     print(s)
+# s = 'I love Moscow'
+# print_paris()
+# print_london()
+# print(s)
+
+def swap(a, b):
+    a, b = b, a
+a = 4
+b = 3
+swap(a, b)
+print(a - b)
