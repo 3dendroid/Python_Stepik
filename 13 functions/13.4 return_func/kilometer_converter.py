ce9ceae6384979e5e9349eecab76b8f253980449
denis sazonov <denis.sazonov@mytona.com>
2025-04-17 17:08:51 +0700

    NEXT: divisors_1.py

--- 13 functions/13.4 return_func/kilometer_converter.py
+++ 13 functions/13.4 return_func/kilometer_converter.py
@@ -1,1 +1,8 @@
-# NEXT
+# объявление функции
+def convert_to_miles(km):
+    miles = km * 0.6214
+    return miles
+# считываем данные
+num = int(input())
+# вызываем функцию
+print(convert_to_miles(num))
