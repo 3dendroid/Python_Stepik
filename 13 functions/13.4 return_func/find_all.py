918bbf0382c20785607a9727e9b0bf313a431e4f
denis sazonov <denis.sazonov@mytona.com>
2025-04-23 09:51:42 +0700

    NEXT: folder 13.5

--- 13 functions/13.4 return_func/find_all.py
+++ 13 functions/13.4 return_func/find_all.py
@@ -1,1 +1,11 @@
-# NEXT
+# объявление функции
+def find_all(target, symbol):
+    return [i for i in range(len(target)) if target[i] == symbol]
+
+
+# считываем данные
+s = input()
+char = input()
+
+# вызываем функцию
+print(find_all(s, char))
