0f8e3534085f43778462cd7f93b5b3474c361628
denis sazonov <denis.sazonov@mytona.com>
2025-05-01 14:43:33 +0700

    NEXT: good_password.py

--- 13 functions/13.5 return_bool_func/next_prime.py
+++ 13 functions/13.5 return_bool_func/next_prime.py
@@ -1,1 +1,22 @@
-# NEXT
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+# объявление функции
+def get_next_prime(num):
+    num += 1
+    while not is_prime(num):
+        num += 1
+    return num
+
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print(get_next_prime(n))
