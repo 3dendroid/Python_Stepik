2da296022b34daa899920ea29e48e7b526904933
denis sazonov <denis.sazonov@mytona.com>
2025-05-07 11:55:08 +0700

    NEXT: palindrom.py

--- 13 functions/13.5 return_bool_func/equal_one.py
+++ 13 functions/13.5 return_bool_func/equal_one.py
@@ -1,1 +1,29 @@
-# NEXT
+# объявление функции
+def is_one_away(word1, word2):
+    if len(word1) != len(word2):
+        return False
+    else:
+        counter = 0
+        for i in range(len(word1)):
+            if word1[i] != word2[i]:
+                counter += 1
+    if counter == 1:
+        return True
+    else:
+        return False
+
+
+# считываем данные
+txt1 = input()
+txt2 = input()
+# вызываем функцию
+print(is_one_away(txt1, txt2))
+
+# # объявление функции
+# def is_one_away(word1, word2):
+#     return len(word1) == len(word2) and len([i for i in range(len(word1)) if word1[i] != word2[i]]) == 1
+# # считываем данные
+# txt1 = input()
+# txt2 = input()
+# # вызываем функцию
+# print(is_one_away(txt1, txt2))
