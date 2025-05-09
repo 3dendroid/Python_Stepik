db93a434b83f06d64a7d2eba8760b4869fe58f2a
denis sazonov <denis.sazonov@mytona.com>
2025-05-09 15:40:32 +0700

    NEXT: BEEGEEK.py

--- 13 functions/13.5 return_bool_func/palindrom.py
+++ 13 functions/13.5 return_bool_func/palindrom.py
@@ -1,6 +1,9 @@
 # объявление функции
 def is_palindrome(text):
-    pass
+    # Оставляем только буквы и цифры, приводим к нижнему регистру
+    clean_text = ''.join(char.lower() for char in text if char.isalnum())
+    return clean_text == clean_text[::-1]
+
 
 # считываем данные
 txt = input()
