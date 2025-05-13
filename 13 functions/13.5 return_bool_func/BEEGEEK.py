e142edbaa2071bc3c1cafa9b9d356971c240e961
denis sazonov <denis.sazonov@mytona.com>
2025-05-13 15:12:46 +0700

    NEXT: correct_bracket_sequence.py

--- 13 functions/13.5 return_bool_func/BEEGEEK.py
+++ 13 functions/13.5 return_bool_func/BEEGEEK.py
@@ -1,1 +1,24 @@
-# NEXT
+def is_valid_password(password):
+    parts = password.split(':')
+
+    if len(parts) != 3:
+        return False
+    try:
+        a, b, c = map(int, parts)
+    except ValueError:
+        return False
+    if str(a) != str(a)[::-1]:
+        return False
+    if b < 2 or any(b % i == 0 for i in range(2, int(b ** 0.5) + 1)):
+        return False
+
+    # Проверка: c — чётное
+    if c % 2 != 0:
+        return False
+    return True
+
+# считываем данные
+psw = input()
+
+# вызываем функцию
+print(is_valid_password(psw))
